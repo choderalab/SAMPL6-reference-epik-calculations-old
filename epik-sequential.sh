8bc6687620302dfbbaab5868dc7a46429c055cf9
Bas Rustenburg <bas.rustenburg@choderalab.org>
2018-02-05 16:48:55 -0500

    Update sequential scripts
    Change sequential run script, and adds initial file for sequential
    analysis.

--- epik-sequential.sh
+++ epik-sequential.sh
@@ -2,17 +2,17 @@
 # to run
 molecule_name="SM01 SM03 SM05 SM07 SM09 SM11 SM13 SM15 SM17 SM19 SM21 SM23 SM02 SM04 SM06 SM08 SM10 SM12 SM14 SM16 SM18 SM20 SM22 SM24"
 
-# Centered around pH 7.0
-phrange="7.0"
+# Centered around pH 7.4
+phrange="7.4"
 
 function run_epik_at_pH {
     file_prefix=$1
     ph=$2
-    mkdir -p output/sequential/${file_prefix}
-    cp input/${file_prefix}.smi output/sequential/${file_prefix}/.
-    pushd output/sequential/${file_prefix}
-    $SCHRODINGER/ligprep -ismi ${file_prefix}.smi -omae ${file_prefix}.mae -WAIT -i 1 # neutralize and store as mae
-    $SCHRODINGER/epik -imae ${file_prefix}.mae -omae ${file_prefix}.epik.${ph}.mae -scan -ph ${ph} -highest_pka 12.0 -lowest_pka 2.0 -WAIT -NO_JOBCONTROL
+    outdir="output/sequential"
+    echo $1 $2
+    mkdir -p ${outdir}/${file_prefix}
+    cp input/${file_prefix}.smi ${outdir}/${file_prefix}/.
+    pushd ${outdir}/${file_prefix}    $SCHRODINGER/epik -imae ${file_prefix}.mae -omae ${file_prefix}.epik.${ph}.mae -scan -ph ${ph} -highest_pka 12.0 -lowest_pka 2.0 -WAIT -NO_JOBCONTROL
 
     # extract atom properties
     $SCHRODINGER/utilities/proplister ${file_prefix}.epik.${ph}.mae -atom_bond_props -o ${file_prefix}.epik.${ph}.prop.csv -c  
@@ -22,5 +22,6 @@ function run_epik_at_pH {
 # make func available to parallel
 export -f run_epik_at_pH
 # Run Epik to enumerate all "reasonable" protomers/tautomers
-parallel -j12 run_epik_at_pH ::: ${molecule_name}  ::: ${phrange} 
-
+avail=`nproc`
+echo running on $avail cores
+parallel -j${avail} run_epik_at_pH ::: ${molecule_name}  ::: ${phrange} 
